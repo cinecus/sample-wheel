e78a7e727523d231a850f771b9a2091799930a67
Chananon Chantaratin <chananon.seen@gmail.com>
2022-04-29 01:15:38 +0700

    first commit

---
+++ data/data.js
@@ -0,0 +1,73 @@
+export const render_data = [
+    {
+        index: 0,
+        name: "Rare Wind",
+        description: "#a8edea → #fed6e3",
+        css: "linear-gradient(135deg, #a8edea 0%, #fed6e3 100%)",
+        height: 80
+    },
+    {
+        index: 1,
+        name: "Saint Petersburg",
+        description: "#f5f7fa → #c3cfe2",
+        css: "linear-gradient(135deg, #c3cfe2 0%, #c3cfe2 100%)",
+        height: 80
+    },
+    {
+        index: 2,
+        name: "Deep Blue",
+        description: "#e0c3fc → #8ec5fc",
+        css: "linear-gradient(135deg, #e0c3fc 0%, #8ec5fc 100%)",
+        height: 80
+    },
+    {
+        index: 3,
+        name: "Ripe Malinka",
+        description: "#f093fb → #f5576c",
+        css: "linear-gradient(135deg, #f093fb 0%, #f5576c 100%)",
+        height: 80
+    },
+    {
+        index: 4,
+        name: "Near Moon",
+        description: "#5ee7df → #b490ca",
+        css: "linear-gradient(135deg, #5ee7df 0%, #b490ca 100%)",
+        height: 80
+    }
+    ,
+    {
+        index: 5,
+        name: "Wild Apple",
+        description: "#d299c2 → #fef9d7",
+        css: "linear-gradient(135deg, #d299c2 0%, #fef9d7 100%)",
+        height: 80
+    }
+    ,
+    {
+        index: 6,
+        name: "test SSS",
+        description: "#4247c2 → #fef1d7",
+        css: "linear-gradient(135deg, #4247c2 0%, #fef1d7 100%)",
+        height: 80
+    }
+];
+
+
+export const name_data = [
+    'James',
+    'Robert',
+    'John',
+    'Michael',
+    'William',
+    'David',
+    'Richard',
+    'Joseph',
+    'Thomas',
+    'Charles',
+    'Christopher',
+    'Daniel',
+    'Matthew',
+    'Anthony',
+    'Mark',
+    'Donald'
+]
